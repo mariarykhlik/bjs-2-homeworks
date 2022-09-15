6d282675b39fdf868b9fce9ef9ec3fb0bb311b9c
mariarykhlik <maria.rykhlik@gmail.com>
2022-09-15 10:56:27 +0300

    2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,57 @@
+"use strict";
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
-
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
-  
+  max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max;
+  min = Infinity;
+  max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
