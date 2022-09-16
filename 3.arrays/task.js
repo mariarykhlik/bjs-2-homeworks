498b78a0dd0b831c26f1720cf83e261e80575e1e
mariarykhlik <maria.rykhlik@gmail.com>
2022-09-16 15:45:13 +0300

    3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,8 @@
+"use strict";
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  return (arr1.length === arr2.length) && (arr1.every((element, index) => element === arr2[index]));
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return arr.filter(number => number > 0).filter(number2 => number2 % 3 === 0).map(number3 => number3 * 10);
 }
