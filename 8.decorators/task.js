7cc6662863fa9ef05761f6b057775c1b4a9f2a74
mariarykhlik <maria.rykhlik@gmail.com>
2022-10-25 09:32:23 +0300

    8.decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,41 @@
+"use strict";
+//Задача №1
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    let hash = args.join(',');
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
+    let result = func(...args);
+    cache.push({hash, result});
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+//Задача №2
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  function wrapper(...args) {
+    wrapper.allCount += 1;
+    if (!timeoutId) {
+      wrapper.count += 1;
+      func(...args);      
+    }
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      wrapper.count += 1;
+      func(...args);
+    }, delay);
+  }
+  wrapper.allCount = 0;
+  wrapper.count = 0;
+  return wrapper;
 }
